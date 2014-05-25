cb8ec97d53091cf5f5611f7f09baa38d54fa04e1
rgirona <C-12323@coiim.es>
2014-05-25 10:39:56 +0200

    Updated template. Function to be evaluated

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,41 @@
 ## functions do
 
 ## Write a short comment describing this function
+## makeCacheMatrix is the constructor: Provides the set y get to cache matrix 
+## structure and inverse  
 
 makeCacheMatrix <- function(x = matrix()) {
+
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmean <- function(solve) m <<- solve
+  getmean <- function() m
+  list(set = set, get = get,
+       setSolve = setSolve,
+       getSolve = getSolve)
+
 
 }
 
 
 ## Write a short comment describing this function
+## Function that either reuse the inverse of matrix cached or  
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getSolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setSolve(m)
+  m
+
+
 }
