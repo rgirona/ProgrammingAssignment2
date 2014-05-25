7b84217245774e9df0ac5226cc53c49d518ea5e2
rgirona <C-12323@coiim.es>
2014-05-25 11:00:37 +0200

    Final version, commnted and ready to evaluate

--- cachematrix.R
+++ cachematrix.R
@@ -3,22 +3,27 @@
 
 ## Write a short comment describing this function
 ## makeCacheMatrix is the constructor: Provides the set y get to cache matrix 
-## structure and inverse  
+## get y set, and to the inverse matrix: getSolve and setSolve  
 
 makeCacheMatrix <- function(x = matrix()) {
 
+## Initialize m (inverse matrix)
   m <- NULL
+
+## constructor set and get   
   set <- function(y) {
     x <<- y
     m <<- NULL
   }
   get <- function() x
-  setmean <- function(solve) m <<- solve
-  getmean <- function() m
+
+## constructor for the cached inverse matrix
+  setSolve <- function(solve) m <<- solve
+  getSolve <- function() m
+
   list(set = set, get = get,
        setSolve = setSolve,
        getSolve = getSolve)
-
 
 }
 
@@ -30,10 +35,13 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
   m <- x$getSolve()
+
+## If already present the cache inverse matrix (m is not null) then just simply return it
   if(!is.null(m)) {
     message("getting cached data")
     return(m)
   }
+## Otherwise calculate the inverse and set the cache inverse matrix
   data <- x$get()
   m <- solve(data, ...)
   x$setSolve(m)
